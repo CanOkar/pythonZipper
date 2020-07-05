d79e630d675d4c16a4115ecaa97ffeab026e1bb2
CanOkar <okar.can@gmail.com>
2020-07-05 22:51:14 +0300

    Update zipper.py

--- zipper.py
+++ zipper.py
@@ -1,28 +1,33 @@
 from datetime import date
-from zipfile import ZipFile, ZIP_DEFLATED
-import glob, datetime, os
+from zipfile import ZipFile, ZIP_LZMA
+import glob, datetime, os, time
 
-doesLogFileExist = glob.glob("*.txt")
-doesLogFileExist = str(doesLogFileExist)
 logFile = open("log.txt", "a") #create a file named "log.txt" if it does not exist
     
 zipFileName = date.today() #zipFileName gonna be name of file that refers current time
 zipFileName = zipFileName.strftime("%d-%m-%Y")
-zipObject = ZipFile(zipFileName, compression=ZIP_DEFLATED, compresslevel=9, mode='w')
 
-files = glob.glob('*.FDB')
+zipObject = ZipFile(zipFileName, compression=ZIP_LZMA, mode='w')
+
+files = glob.glob('*.exe')
 logFile = open("log.txt", "a")
-logFile.write("Sıkıştırma işlemi " + str(datetime.datetime.now()) + "itibariyle başladı.\n")
+logFile.write("Compression process has started on " + str(datetime.datetime.now()) + "\n")
 logFile.close()
-print("Sıkıştırma işlemi " , str(datetime.datetime.now()) , "itibariyle başladı.")
+
+print("-------------------------------------------------------------------------------")
+print("Compression process has started on " + str(datetime.datetime.now()))
+print("-------------------------------------------------------------------------------")
 
 for n in files:
     zipObject.write(n)
+zipObject.close()
 
 os.rename(zipFileName , zipFileName + ".zip") #add .zip extension to file
+
 logFile = open("log.txt", "a")
-logFile.write("Sıkıştırma işlemi " + str(datetime.datetime.now()) + "itibariyle tamamlandı.\n")
+logFile.write("Compression process has finished on " + str(datetime.datetime.now()) + "\n")
 logFile.close()
-print("Sıkıştırma işlemi " + str(datetime.datetime.now()) + "itibariyle tamamlandı.")
 
-
+print("-------------------------------------------------------------------------------")
+print("Compression process has finished on " +str(datetime.datetime.now()))
+print("-------------------------------------------------------------------------------")
