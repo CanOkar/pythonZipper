986aa4154fa1a05d5d5b42bec50dbeb2ffbc0a1c
CanOkar <okar.can@gmail.com>
2020-07-05 16:04:27 +0300

    Update zipper.py

--- zipper.py
+++ zipper.py
@@ -1,13 +1,28 @@
 from datetime import date
-from zipfile import ZipFile
-import glob
+from zipfile import ZipFile, ZIP_DEFLATED
+import glob, datetime, os
 
-#zipFileName gonna be name of file that refers current time
-zipFileName = date.today()
+doesLogFileExist = glob.glob("*.txt")
+doesLogFileExist = str(doesLogFileExist)
+logFile = open("log.txt", "a") #create a file named "log.txt" if it does not exist
+    
+zipFileName = date.today() #zipFileName gonna be name of file that refers current time
 zipFileName = zipFileName.strftime("%d-%m-%Y")
-zipObject = ZipFile(zipFileName , 'w')
+zipObject = ZipFile(zipFileName, compression=ZIP_DEFLATED, compresslevel=9, mode='w')
 
 files = glob.glob('*.FDB')
+logFile = open("log.txt", "a")
+logFile.write("Sıkıştırma işlemi " + str(datetime.datetime.now()) + "itibariyle başladı.\n")
+logFile.close()
+print("Sıkıştırma işlemi " , str(datetime.datetime.now()) , "itibariyle başladı.")
 
 for n in files:
     zipObject.write(n)
+
+os.rename(zipFileName , zipFileName + ".zip") #add .zip extension to file
+logFile = open("log.txt", "a")
+logFile.write("Sıkıştırma işlemi " + str(datetime.datetime.now()) + "itibariyle tamamlandı.\n")
+logFile.close()
+print("Sıkıştırma işlemi " + str(datetime.datetime.now()) + "itibariyle tamamlandı.")
+
+
